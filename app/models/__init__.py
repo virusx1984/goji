ef2df015cc7690a81690f8c38354fce306334035
Lining <virusx1984@gmail.com>
2025-08-10 21:07:11 +0800

    master_data_models.py 名字错了 改正它

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -8,6 +8,6 @@ from .base_model import ModelBase
 from .organization_models import *
 from .user_permission_models import *
 # Import other model files as you create them
-# from .master_data_models import *
+from .master_data_models import *
 # from .process_models import *
 # from .demand_models import *
